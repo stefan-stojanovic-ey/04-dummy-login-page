4f1335436af23d88e06ba25acc6cae19e97cf8ab
Stefan Stojanovic <Stefan.Stojanovic2@rs.ey.com>
2022-07-22 10:54:23 +0200

    Added useEffect Hook to validate form submits

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,42 +1,57 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Card from '../UI/Card/Card';
-import classes from './Login.module.css';
-import Button from '../UI/Button/Button';
+import Card from "../UI/Card/Card";
+import classes from "./Login.module.css";
+import Button from "../UI/Button/Button";
 
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredEmail, setEnteredEmail] = useState("");
   const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredPassword, setEnteredPassword] = useState("");
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
-  const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+  useEffect(() => {
+    const validEmail = enteredEmail.includes("@");
+    const validPass = enteredPassword.length > 6;
 
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
+    setFormIsValid(validEmail && validPass);
+  }, [enteredEmail, enteredPassword]);
+
+  // validacija email-a
+  function validEmailCheck(val) {
+    return val.includes("@");
+  }
+
+  // validacija password-a
+  function validPassCheck(val) {
+    return val > 6;
+  }
+
+  const emailChangeHandler = (e) => {
+    const val = e.target.value;
+
+    setEnteredEmail(val);
+    setEmailIsValid(validEmailCheck(val));
   };
 
-  const passwordChangeHandler = (event) => {
-    setEnteredPassword(event.target.value);
+  const passwordChangeHandler = (e) => {
+    const val = e.target.value;
 
-    setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@')
-    );
+    setEnteredPassword(val);
+    setPasswordIsValid(validPassCheck(val));
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    setEmailIsValid(validEmailCheck(enteredEmail));
   };
 
   const validatePasswordHandler = () => {
     setPasswordIsValid(enteredPassword.trim().length > 6);
   };
 
-  const submitHandler = (event) => {
-    event.preventDefault();
+  const submitHandler = (e) => {
+    e.preventDefault();
     props.onLogin(enteredEmail, enteredPassword);
   };
 
@@ -45,7 +60,7 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
+            emailIsValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="email">E-Mail</label>
@@ -59,7 +74,7 @@ const Login = (props) => {
         </div>
         <div
           className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
+            passwordIsValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="password">Password</label>
